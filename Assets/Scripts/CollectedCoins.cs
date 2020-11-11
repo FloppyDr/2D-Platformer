3dc0b55df66aa104f8b5bc4287f8ed07820842e3
FloppyDr <DimaAlexandrov@yandex.ru>
2020-11-12 01:28:09 +0300

    Init

---
+++ Assets/Scripts/CollectedCoins.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectedCoins : MonoBehaviour
+{
+    [SerializeField] private Text text;
+
+    private AudioSource _coinSound;
+    private Coin[] coins;
+    private int _collectedCoins;
+
+    private void OnEnable()
+    {
+        coins = gameObject.GetComponentsInChildren<Coin>();
+
+        foreach (var coin in coins)
+        {
+            coin.Collected += OnCoinCollected;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (var coin in coins)
+        {
+            coin.Collected -= OnCoinCollected;
+        }
+    }
+
+    private void OnCoinCollected()
+    {
+        _collectedCoins++;
+        text.text = _collectedCoins.ToString();
+    }
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField] private Transform _startPos;
+    [SerializeField] private Transform _endPos;
+
+    private float _patrolTime = 1f;
+    private Tweener _tween;
+
+    private void Start()
+    {
+        _tween = transform.DOMove(_startPos.position, _patrolTime).SetAutoKill(false);
+        _tween.SetEase(Ease.Linear);
+    }
+
+    private void Update()
+    {
+        if (_startPos.position == gameObject.transform.position)
+        {
+            _tween.ChangeEndValue(_endPos.position, true).Restart();
+        }
+        else if (_endPos.position == gameObject.transform.position)
+        {
+            _tween.ChangeEndValue(_startPos.position, true).Restart();
+        }
+    }
+}
